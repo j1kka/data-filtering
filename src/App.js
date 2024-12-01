cf981985666856ef48b1abb2f4f5a0d48d492717
j1kka <zhyldyz.kanatbekova@alatoo.edu.kg>
2024-12-01 21:57:44 +0600

    Initialize data using useEffect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+
 function App() {
   const [data, setData] = useState([]); 
   const [search, setSearch] = useState(''); 
 
+  useEffect(() => {
+    const fetchData = async () => {
+      const fakeData = [
+        { id: 1, name: 'Frontend' },
+        { id: 2, name: 'Programming language' },
+        { id: 3, name: 'Database' },
+        { id: 4, name: 'Introduction to engineering' },
+        { id: 5, name: 'Calculus' },
+        { id: 6, name: 'Algebra' },
+        { id: 7, name: 'Kyrgyz language' },
+        { id: 8, name: 'OOP' },
+        { id: 9, name: 'Ethical hacking' },
+        { id: 10, name: 'Physical education' },
+      ];
+      setData(fakeData);
+    };
+  
+    fetchData();
+  }, []);
+  
+
   return (
     <div>
-      <h1>Data Filtering App</h1>
+      <h1>Filtering Subjects</h1>
     </div>
   );
 }
