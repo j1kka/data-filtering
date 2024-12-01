078566635048a30a3dd58fed34236f66b4c714f1
j1kka <zhyldyz.kanatbekova@alatoo.edu.kg>
2024-12-01 21:45:12 +0600

    Add state for data and search string

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 function App() {
+  const [data, setData] = useState([]); 
+  const [search, setSearch] = useState(''); 
+
   return (
     <div>
       <h1>Data Filtering App</h1>
     </div>
   );
 }
-
-export default App;
